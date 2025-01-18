bc26266b5d2fb1f8d96d44fa009640241a2d66cd
stacee <stacee.salinas@csuglobal.edu>
2025-01-18 12:02:49 -0500

    CSC450

---
+++ Module1/Module1/Module1.cpp
@@ -0,0 +1,27 @@
+// Module1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string firstName = "Jonathon";
+    string lastName = "Morant";
+    string homeStreet = "6017 Homefront Avenue";
+    string homeCity = "Valintia";
+    int zipCode = 99999;
+
+
+    cout << "Citizen information:"
+        << endl
+        << firstName + " " + lastName
+        << endl
+        << "Address: " + homeStreet + ", " + homeCity
+        << endl
+        << "Zipcode: " + zipCode;
+
+    return 0;
+}
---
+++ Module1/Syntax_Error_1.cpp
@@ -0,0 +1,26 @@
+// Syntax_Error_1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+/* Simple Program with a few Errors for Correction
+   Please be sure to correct all outlined errors.
+*/
+
+#include<iostream>
+#include<conio.h>
+
+//Standard namespace declaration
+using namespace std;
+
+//Main Function
+int main()
+{
+
+    //Standard Ouput Statement
+    cout << "Welcome to this C++ Program." << endl;
+
+    cout << "I have corrected all errors for this program." << endl;
+
+    // Wait For Output Screen
+
+    //Main Function return Statement
+    return 0;
+}
